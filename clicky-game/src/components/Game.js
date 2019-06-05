ab8b4340a1ac11d6195d1b63e307f098a7b89781
Brittany F <brittfranklin92@gmail.com>
2019-06-05 16:35:16 -0400

    Component updates

---
+++ clicky-game/src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function Game() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Game;
---
+++ clicky-game/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>Looney Tunes Clicky Game</h1>
+      <h3>Click on an Image to Start</h3>
+      <h3>Score: </h3>
+    </div>
+  )
+}
+
+export default Header;
