2739e27b8e047d8bafea268b8470818be13ef9b9
Brittany Franklin <brittanyfranklin@users-MacBook-Pro.local>
2019-06-15 21:01:07 -0400

    Updates

---
+++ precache-manifest.1345d5181b98ac3bca2f6ee9a30498a1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745533e54bad52f164ac10420fc3c6e7",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0ebd0686798a4e2895ff",
+    "url": "/Clicky-Game/static/css/main.b94d1e1e.chunk.css"
+  },
+  {
+    "revision": "a7e0f5d4c57d5a18e9c5",
+    "url": "/Clicky-Game/static/js/2.70c33071.chunk.js"
+  },
+  {
+    "revision": "0ebd0686798a4e2895ff",
+    "url": "/Clicky-Game/static/js/main.81996b32.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "32eb0f73b0b6ef765a4261cca7671c31",
+    "url": "/Clicky-Game/static/media/bugs-bunny.32eb0f73.png"
+  },
+  {
+    "revision": "4727813ddcbd07fd5557edabc5e791fc",
+    "url": "/Clicky-Game/static/media/daffy-duck.4727813d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9dff47cd5efa840395d4a55a9a65b989",
+    "url": "/Clicky-Game/static/media/marvin.9dff47cd.png"
+  },
+  {
+    "revision": "22d0f01ceb4267b6714c66114be84969",
+    "url": "/Clicky-Game/static/media/roadrunner.22d0f01c.png"
+  },
+  {
+    "revision": "1263c70ea66ae35ea80d625a300723f6",
+    "url": "/Clicky-Game/static/media/tas.1263c70e.png"
+  },
+  {
+    "revision": "f9305f1e600cfe9be238f28b314674ee",
+    "url": "/Clicky-Game/static/media/tweety.f9305f1e.png"
+  },
+  {
+    "revision": "7b6f20ff0161724c505fb170fe47588a",
+    "url": "/Clicky-Game/static/media/yosemite-sam.7b6f20ff.png"
+  }
+]);
